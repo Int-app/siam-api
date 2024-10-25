4359996617dcf5f7b3dad937038cf53e5ff0681e
liubin <41326097@qq.com>
2024-10-25 13:32:43 +0900

    commit

--- src/main/java/jp/co/siam/restapi/controller/InsuranceContractInfoController.java
+++ src/main/java/jp/co/siam/restapi/controller/InsuranceContractInfoController.java
@@ -5,14 +5,17 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import jp.co.siam.restapi.common.ResponseCode;
 import jp.co.siam.restapi.common.ResponseResult;
+import jp.co.siam.restapi.entity.Employeeinfo;
 import jp.co.siam.restapi.entity.Insurancecontractinfo;
 import jp.co.siam.restapi.entity.InsurancecontractinfoFindParam;
+import jp.co.siam.restapi.service.EmployerService;
 import jp.co.siam.restapi.service.InsuranceContractInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @Api(tags = "insurance interface")
@@ -22,11 +25,28 @@ public class InsuranceContractInfoController {
   @Autowired
   InsuranceContractInfoService insuranceContractInfoService;
 
+    @Autowired
+    EmployerService employerService;
+
    @PostMapping("/get/{insurancecontractid}")
    @ApiOperation("get")
     public ResponseResult<Insurancecontractinfo> getInsuranceContractInfos( @PathVariable("insurancecontractid") Integer insurancecontractid) {
         Insurancecontractinfo insurancecontractinfo= insuranceContractInfoService.getInsurancecontractinfo(insurancecontractid);
+        setEmployeeName(insurancecontractinfo);
         return new ResponseResult(ResponseCode.SUCCESS, "",insurancecontractinfo);
+    }
+
+    private void setEmployeeName(Insurancecontractinfo insurancecontractinfo) {
+        String employeeId = insurancecontractinfo.getEmployeeid();
+        String teamemployeeId = insurancecontractinfo.getTeamemployeeid();
+        Employeeinfo employeeinfo = employerService.getEmployeeinfo(employeeId);
+        if(!Objects.isNull(employeeinfo)){
+            insurancecontractinfo.setEmployeeName(employeeinfo.getEmployeename());
+        }
+        Employeeinfo teamemployeeinfo = employerService.getEmployeeinfo(teamemployeeId);
+        if(!Objects.isNull(teamemployeeinfo)){
+            insurancecontractinfo.setTeamemployeeName(teamemployeeinfo.getEmployeename());
+        }
     }
 
     @PostMapping("/get_list_bypage")
